6a66a13576c49f19035c3f7650ad11602d4bcbb0
Andreas Iosifelis <aiosifelis@architech.ca>
2024-10-20 22:29:12 -0400

    install templ for views

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/aiosifelis/go-web-app-template/src/middlewares"
 	"github.com/aiosifelis/go-web-app-template/src/system"
+	"github.com/aiosifelis/go-web-app-template/src/views/layouts"
+	"github.com/aiosifelis/go-web-app-template/src/views/pages"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -29,8 +31,6 @@ func main() {
 	e.Use(middleware.Recover())
 	e.Use(middlewares.Database(&connectionPool))
 
-	e.Renderer = system.NewTemplateRenderer()
-
 	e.HTTPErrorHandler = func(err error, c echo.Context) {
 		c.Logger().Error(err)
 		// c.Render(http.StatusInternalServerError, "error.html", map[string]interface{}{
@@ -43,36 +43,50 @@ func main() {
 	e.Static("public", "src/public")
 
 	authRoutes := e.Group("/auth")
-	mainRoutes := e.Group("/main")
+	mainRoutes := e.Group("/")
 
 	mainRoutes.Use(middlewares.AuthMiddleware)
 
 	mainRoutes.GET("/about", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "about.html", map[string]interface{}{
-			"Title":   "About Page",
-			"Message": "Welcome to the About",
-		})
+		return system.RenderTemplate(c, pages.AboutPage(&layouts.PageData{
+			Title:   "About Page",
+			Content: "This is the about page",
+		}))
 	})
 
 	authRoutes.GET("/login", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "auth/login.html", map[string]interface{}{
-			"Title":   "Login Page",
-			"Message": "Login to your account",
-		})
+		return system.RenderTemplate(c, pages.LoginPage(&layouts.PageData{
+			Title:   "Login Page",
+			Content: "This is the login page",
+		}))
+	})
+
+	authRoutes.GET("/signup", func(c echo.Context) error {
+		return system.RenderTemplate(c, pages.SignupPage(&layouts.PageData{
+			Title:   "Signup Page",
+			Content: "This is the signup page",
+		}))
+	})
+
+	authRoutes.GET("/forgot-password", func(c echo.Context) error {
+		return system.RenderTemplate(c, pages.ForgotPasswordPage(&layouts.PageData{
+			Title:   "Forgot Password Page",
+			Content: "This is the forgot password page",
+		}))
 	})
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "home.html", map[string]interface{}{
-			"Title":   "Home Page",
-			"Message": "Welcome to the Home Page",
-		})
+		return system.RenderTemplate(c, pages.HomePage(&layouts.PageData{
+			Title:   "Home Page",
+			Content: "This is the home page",
+		}))
 	})
 
 	e.GET("/error", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "error.html", map[string]interface{}{
-			"Title":   "Error Page",
-			"Message": "An error occurred",
-		})
+		return system.RenderTemplate(c, pages.ErrorPage(&layouts.PageData{
+			Title:   "Error Page",
+			Content: "This is the error page",
+		}))
 	})
 
 	e.Logger.Fatal(e.Start(":3000"))
